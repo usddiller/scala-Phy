40e7299fb99641722457b07310e5e2a3fa5215ee
Usddiller <grutt2007@gmail.com>
2026-05-02 14:04:54 +0500

    waiter+ resource ierarchy

--- src/main/scala/PhySamWithForks.scala
+++ src/main/scala/PhySamWithForks.scala
@@ -1,71 +1,97 @@
-import cats.effect.{IO, IOApp, Ref}
-import cats.syntax.all.*
-import scala.concurrent.duration.DurationInt
-object PhySamWithForks extends IOApp.Simple {
-  trait ForkState
-  case object OnTable extends ForkState
-  case class InHand(philosopherId: Int) extends ForkState
-  case class Fork(id: Int, state: ForkState)
+import cats.effect._
+import cats.syntax.all._
+import scala.concurrent.duration._
 
-  trait PhyState
-  object Eat extends PhyState
-  object Think extends PhyState
-  case class Phy(id: Int, state: PhyState)
+object DiningPhilosophersHierarchy extends IOApp.Simple {
 
+  case class Fork(id: Int, isTaken: Ref[IO, Boolean])
 
-  case class TableState(philosophers: Vector[Phy], forks: Vector[Fork])
+  case class Philosopher(id: Int, firstFork: Fork, secondFork: Fork, count: Ref[IO, Int]) {
 
-  object Waiter {
+    def tryToEat: IO[Boolean] = {
+      for {
+        firstTaken <- firstFork.isTaken.modify {
+          case false => (true, true)
+          case true => (true, false)
+        }
+        res <- if (firstTaken) {
+          secondFork.isTaken.modify {
+            case false => (true, true)
+            case true => (true, false)
+          }.flatMap {
+            case true => IO.pure(true)
+            case false => firstFork.isTaken.set(false).as(false)
+          }
+        } else IO.pure(false)
+      } yield res
+    }
 
-    def tryEat(stateRef: Ref[IO, TableState], phyId: Int): IO[Boolean] =
-      stateRef.modify { s =>
-
-        val leftIdx = phyId - 1
-        val rightIdx = phyId % 5
-        val leftFork = s.forks(leftIdx)
-        val rightFork = s.forks(rightIdx)
-        
-        if (leftFork.state == OnTable && rightFork.state == OnTable) {
-          val updatedForks = s.forks
-            .updated(leftIdx, leftFork.copy(state = InHand(phyId)))
-            .updated(rightIdx, rightFork.copy(state = InHand(phyId)))
-
-          val updatedPhys = s.philosophers.updated(phyId - 1, Phy(phyId, Eat))
-
-          (s.copy(philosophers = updatedPhys, forks = updatedForks), true)
+    def eat: IO[Unit] = {
+      for {
+        canEat <- tryToEat
+        _ <- if (canEat) {
+          for {
+            _ <- count.update(_ + 1)
+            c <- count.get
+            _ <- IO.println(s"$id ЕСТ ($c раз)")
+            _ <- IO.sleep(1.second)
+            _ <- firstFork.isTaken.set(false)
+            _ <- secondFork.isTaken.set(false)
+            _ <- IO.println(s"$id закончил есть")
+          } yield ()
         } else {
-          (s, false)
+          IO.sleep(50.millis) >> eat
         }
-      }
+      } yield ()
+    }
 
-    def backToThink(stateRef: Ref[IO, TableState], phyId: Int): IO[Unit] =
-      stateRef.update { s =>
-        val leftIdx = phyId - 1
-        val rightIdx = phyId % 5
-
-        val updatedForks = s.forks
-          .updated(leftIdx, s.forks(leftIdx).copy(state = OnTable))
-          .updated(rightIdx, s.forks(rightIdx).copy(state = OnTable))
-
-        val updatedPhys = s.philosophers.updated(phyId - 1, Phy(phyId, Think))
-
-        s.copy(philosophers = updatedPhys, forks = updatedForks)
-      }
-  }
-  
-  def tableNow(phys: List[Phy]): IO[Unit] = {
-    IO.println("--- Состояние стола на данный момент ---") >>
-      phys.traverse { p =>
-        val status = p.state match {
-          case Eat => "ест"
-          case Think => "думает"
-        }
-        IO.println(s"Философ ${p.id} - $status")
-      }.void >>
-      IO.println("---------------------------------------")
+    def live: IO[Unit] = {
+      for {
+        _ <- IO.println(s"$id думает...")
+        _ <- IO.sleep(500.millis)
+        _ <- eat
+        _ <- live
+      } yield ()
+    }
   }
 
+  override def run: IO[Unit] =
+    for {
 
-  override def run: IO[Unit] = ???
+      f1 <- Ref.of[IO, Boolean](false); f2 <- Ref.of[IO, Boolean](false)
+      f3 <- Ref.of[IO, Boolean](false); f4 <- Ref.of[IO, Boolean](false)
+      f5 <- Ref.of[IO, Boolean](false)
 
+      c1 <- Ref.of[IO, Int](0); c2 <- Ref.of[IO, Int](0)
+      c3 <- Ref.of[IO, Int](0); c4 <- Ref.of[IO, Int](0)
+      c5 <- Ref.of[IO, Int](0)
+
+
+      fork1 = Fork(1, f1); fork2 = Fork(2, f2); fork3 = Fork(3, f3)
+      fork4 = Fork(4, f4); fork5 = Fork(5, f5)
+
+      philosophers = List(
+        Philosopher(1, fork1, fork2, c1),
+        Philosopher(2, fork2, fork3, c2),
+        Philosopher(3, fork3, fork4, c3),
+        Philosopher(4, fork4, fork5, c4),
+        Philosopher(5, fork1, fork5, c5)
+      )
+
+      _ <- philosophers.traverse(_.live.start)
+
+
+      _ <- (for {
+        _ <- IO.sleep(15.seconds)
+        _ <- IO.println("")
+        _ <- philosophers.traverse { p =>
+          for {
+            c <- p.count.get
+            _ <- IO.println(s"${p.id} поел $c раз")
+          } yield ()
+        }
+
+      } yield ()).foreverM.start
+      _ <- IO.never
+    } yield ()
 }
---
+++ src/main/scala/PhyWaiter.scala
@@ -0,0 +1,107 @@
+import cats.effect._
+import cats.syntax.all._
+import scala.concurrent.duration._
+import cats.effect.std.Semaphore
+
+object PhyWaiter extends IOApp.Simple {
+
+  case class Fork(id: Int, isTaken: Ref[IO, Boolean])
+
+  case class Waiter(places: Semaphore[IO])
+  //если честно я не особо разобралс что такое семафор
+  // со слов ии это просто счетчик ограничивающий количество философов пытающихся обращаться
+  // к нему, но я так и не понял как мне самостоятельно его написать
+  // не разобрался с атомарностью  и семантической блокировкой
+  // а именно не понял как сделать чтоб там и очеред была, и паралельно оно выполнялось, чтоб
+  //философы одновременно не уменьшили счетчик
+
+  case class Philosopher(id: Int, leftFork: Fork, rightFork: Fork, count: Ref[IO, Int], waiter: Waiter) {
+    def tryToEat: IO[Boolean] = {
+      for {
+        leftTaken <- leftFork.isTaken.modify {
+          case false => (true, true)
+          case true => (true, false)
+        }
+        res <- if (leftTaken) {
+          rightFork.isTaken.modify {
+            case false => (true, true)
+            case true => (true, false)
+          }.flatMap {
+            case true => IO.pure(true)
+            case false => leftFork.isTaken.set(false).as(false)
+          }
+        } else IO.pure(false)
+      } yield res
+    }
+
+    def eat: IO[Unit] = {
+      for {
+        canEat <- tryToEat
+        _ <- if (canEat) {
+          for {
+            _ <- count.update(_ + 1)
+            c <- count.get
+            _ <- IO.println(s"$id ЕСТ ($c раз)")
+            _ <- IO.sleep(1.second)
+            _ <- leftFork.isTaken.set(false)
+            _ <- rightFork.isTaken.set(false)
+            _ <- IO.println(s"$id закончил есть")
+          } yield ()
+        } else {
+          IO.sleep(50.millis) >> eat
+        }
+      } yield ()
+    }
+
+
+    def live: IO[Unit] = {
+      for {
+        _ <- IO.println(s"Философ $id думает...")
+        _ <- IO.sleep(500.millis)
+        _ <- waiter.places.acquire
+        _ <- eat
+        _ <- waiter.places.release
+        _ <- live
+      } yield ()
+    }
+  }
+
+  override def run: IO[Unit] = {
+    for {
+      sem <- Semaphore[IO](4)
+      waiter = Waiter(sem)
+
+      f1 <- Ref.of[IO, Boolean](false); f2 <- Ref.of[IO, Boolean](false)
+      f3 <- Ref.of[IO, Boolean](false); f4 <- Ref.of[IO, Boolean](false)
+      f5 <- Ref.of[IO, Boolean](false)
+
+      c1 <- Ref.of[IO, Int](0); c2 <- Ref.of[IO, Int](0)
+      c3 <- Ref.of[IO, Int](0); c4 <- Ref.of[IO, Int](0)
+      c5 <- Ref.of[IO, Int](0)
+
+      philosophers = List(
+        Philosopher(1, Fork(1, f1), Fork(2, f2), c1, waiter),
+        Philosopher(2, Fork(2, f2), Fork(3, f3), c2, waiter),
+        Philosopher(3, Fork(3, f3), Fork(4, f4), c3, waiter),
+        Philosopher(4, Fork(4, f4), Fork(5, f5), c4, waiter),
+        Philosopher(5, Fork(5, f5), Fork(1, f1), c5, waiter)
+      )
+
+
+      _ <- philosophers.traverse(_.live.start)
+
+      _ <- (for {
+        _ <- IO.sleep(15.seconds)
+        _ <- IO.println("")
+        _ <- philosophers.traverse { p =>
+          for {
+            c <- p.count.get
+            _ <- IO.println(s"${p.id} поел $c раз")
+          } yield ()
+        }
+
+      } yield ()).foreverM.start
+
+      _ <- IO.never
+    } yield ()  }
+}
